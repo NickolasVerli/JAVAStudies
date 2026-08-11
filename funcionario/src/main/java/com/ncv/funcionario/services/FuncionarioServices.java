72983d49a62d96c6f9e1399b97141944f51a54e5
Nickolas Verli <nickolasverli@outlook.com.br>
2026-08-10 21:51:55 -0300

    creation of services

--- funcionario/src/main/java/com/ncv/funcionario/services/FuncionarioServices.java
+++ funcionario/src/main/java/com/ncv/funcionario/services/FuncionarioServices.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
 @Service
 public class FuncionarioServices {
     @Autowired
@@ -22,5 +24,17 @@ public class FuncionarioServices {
 
     public FuncionarioModel criarPessoa(FuncionarioModel funcionarioModel){
         return funcionarioRepository.save(funcionarioModel);
+    }
+    //DELETAR
+    public void deletar(Long id){
+        funcionarioRepository.deleteById(id);
+    }
+
+    //update
+    public  FuncionarioModel atualizar(Long id, FuncionarioModel funcionarioModel){
+        FuncionarioModel func = funcionarioRepository.findById(id).get();
+        func.setNome(funcionarioModel.getNome());
+        func.setCpf(funcionarioModel.getCpf());
+        return funcionarioRepository.save(func);
     }
 }
